ec27284dd1f79e4d218120c3bc71e10216eaacdb
maxs24 <ksenofontov-maksim@mail.ru>
2022-12-15 22:23:01 +0300

    Add task 2

--- Exercise_1/Ex1_main.py
+++ Exercise_1/Ex1_main.py
@@ -1,1 +1,85 @@
+import matplotlib.pyplot as plt
+
+
+def get_data_from_file(file_name):
+    data_one = []
+    data_two = []
+
+    with open(file_name, 'r') as file:
+        for line in file:
+            values = []
+            mas_line = line.split(" ")
+            for elem in mas_line:
+                if elem != '':
+                    values.append(elem)
+            data_one.append(float(values[0]))
+            data_two.append(float(values[1]))
+
+    return data_one, data_two
+
+
+# Выборочное среднее
+def get_mean_data(data):
+    return sum(data)/len(data)
+
+
+# Выборочная дисперсия
+def get_variance_data(data):
+    mean = get_mean_data(data)
+    sum = 0
+    for elem in data:
+        sum += (mean - elem)**2
+    return sum/len(data)
+
+
+# Стандартное отклонение
+def get_standart_dev_data(data):
+    return get_variance_data(data) ** (0.5)
+
+
+# Выборочный коэффициент асимметрии
+def get_asymmetry_data(data):
+    mean = get_mean_data(data)
+    standart_dev = get_standart_dev_data(data)
+    sum = 0
+    for elem in data:
+        sum += (elem - mean) ** 3
+    return sum / standart_dev ** 3
+
+
+def get_kurtosis_data(data):
+    mean = get_mean_data(data)
+    standart_dev = get_standart_dev_data(data)
+    sum = 0
+    for elem in data:
+        sum += (elem - mean) ** 4
+    return sum / standart_dev ** 4 - 3
+
+
+# Построение гистограммы
+def draw_histogram(data_one, data_two):
+    fix, ax = plt.subplots(1, 2)
+    ax[0].hist(data_one, bins=len(data_one))
+    ax[0].set_title('Гистограмма первой выборки')
+    ax[1].hist(data_two, bins=len(data_two))
+    ax[1].set_title('Гистограмма второй выборки')
+    plt.show()
+
+
+if __name__ == '__main__':
+    FILE_NAME = 'Ex1_DAT8.TXT'
+
+    data_one, data_two = get_data_from_file(FILE_NAME)
+
+    print('Выборочное среднее для первой выборки: ' + str(get_mean_data(data_one)))
+    print('Выборочная дисперсия для первой выборки: ' + str(get_variance_data(data_one)))
+    print('Выборочный коэффициент асимметрии для первой выборки: ' + str(get_asymmetry_data(data_one)))
+    print('Выборочный коэффициент эксцесса для первой выборки: ' + str(get_kurtosis_data(data_one)) + '\n')
+
+    print('Выборочное среднее для второй выборки: ' + str(get_mean_data(data_two)))
+    print('Выборочная дисперсия для второй выборки: ' + str(get_variance_data(data_two)))
+    print('Выборочный коэффициент асимметрии для первой выборки: ' + str(get_asymmetry_data(data_two)))
+    print('Выборочный коэффициент эксцесса для второй выборки: ' + str(get_kurtosis_data(data_two)))
+
+    draw_histogram(data_one, data_two)
 
--- Exercise_2/Ex2_main.py
+++ Exercise_2/Ex2_main.py
@@ -1,1 +1,38 @@
+from scipy.stats import chisquare
 
+def get_data_from_file(file_name):
+    data_one = []
+    data_two = []
+
+    with open(file_name, 'r') as file:
+        for line in file:
+            values = []
+            mas_line = line.split(" ")
+            for elem in mas_line:
+                if elem != '':
+                    values.append(elem)
+            data_one.append(float(values[0]))
+            data_two.append(float(values[1]))
+
+    return data_one, data_two
+
+def is_normal(data, phi):
+    statistic, p = chisquare(data)
+    print("p-значение: ", p)
+    if p > phi:
+        print("Распределение выборки является нормальным")
+    else:
+        print("Распределение выборки не является нормальным")
+
+if __name__ == '__main__':
+    FILE_NAME = 'Ex2_DAT8.TXT'
+    PHI = 0.05
+
+    data_one, data_two = get_data_from_file(FILE_NAME)
+
+    print("Проверка первой выборки")
+    is_normal(data_one, PHI)
+    print("\n")
+
+    print("Проверка второй выборки")
+    is_normal(data_two, PHI)
