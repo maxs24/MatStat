627c138578e1dec5d3d9acf3bc02af1f56b25d3a
maxs24 <ksenofontov-maksim@mail.ru>
2022-12-17 20:42:21 +0300

    Add part of task 6

--- Exercise_6/Ex6_main.py
+++ Exercise_6/Ex6_main.py
@@ -1,1 +1,23 @@
+import scipy
+import statistics
+from MatStat import workWithFile
+import numpy
+
+
+def run(file_name, phi):
+    data_1, data_2 = workWithFile.get_data_from_file_vertical(file_name)
+
+    interval_1 = scipy.stats.norm.interval(confidence=1.0 - phi, loc=statistics.mean(data_1), scale=numpy.std(data_1))
+    interval_2 = scipy.stats.norm.interval(confidence=1.0 - phi, loc=statistics.mean(data_2), scale=numpy.std(data_2))
+    print("Доверительный интервал среднего для первой выборки: ")
+    print(interval_1)
+    print("Доверительный интервал среднего для второй выборки: ")
+    print(interval_2)
+
+
+if __name__=='__main__':
+    FILE_NAME = "Ex6_DAT8.TXT"
+    PHI = 0.05
+
+    run(FILE_NAME, PHI)
 
