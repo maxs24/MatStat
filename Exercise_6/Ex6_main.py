4fdf11553f1a7269a452cd55084604cd4432287c
maxs24 <ksenofontov-maksim@mail.ru>
2022-12-19 22:56:43 +0300

    Complete task 6

--- Exercise_6/Ex6_main.py
+++ Exercise_6/Ex6_main.py
@@ -3,19 +3,32 @@ import statistics
 from MatStat import workWithFile
 import numpy
 
+def get_interval_std(data, phi):
+    var = statistics.variance(data)
+    hi_1 = scipy.stats.chi2.ppf(phi, len(data))
+    hi_2 = scipy.stats.chi2.ppf(1 - phi, len(data))
+    return (len(data)*var/hi_2, len(data)*var/hi_1)
 
 def run(file_name, phi):
     data_1, data_2 = workWithFile.get_data_from_file_vertical(file_name)
 
-    interval_1 = scipy.stats.norm.interval(confidence=1.0 - phi, loc=statistics.mean(data_1), scale=numpy.std(data_1))
-    interval_2 = scipy.stats.norm.interval(confidence=1.0 - phi, loc=statistics.mean(data_2), scale=numpy.std(data_2))
+    interval_mean_1 = scipy.stats.norm.interval(confidence=1.0 - phi, loc=statistics.mean(data_1), scale=numpy.std(data_1))
+    interval_mean_2 = scipy.stats.norm.interval(confidence=1.0 - phi, loc=statistics.mean(data_2), scale=numpy.std(data_2))
+    interval_std_1 = get_interval_std(data_1, phi)
+    interval_std_2 = get_interval_std(data_2, phi)
     print("Доверительный интервал среднего для первой выборки: ")
-    print(interval_1)
+    print(interval_mean_1)
+    print("Доверительный интервал дисперсии для первой выборки: ")
+    print(interval_std_1)
+    print("\n")
     print("Доверительный интервал среднего для второй выборки: ")
-    print(interval_2)
+    print(interval_mean_2)
+    print("Доверительный интервал дисперсии для второй выборки: ")
+    print(interval_std_2)
 
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     FILE_NAME = "Ex6_DAT8.TXT"
     PHI = 0.05
 
